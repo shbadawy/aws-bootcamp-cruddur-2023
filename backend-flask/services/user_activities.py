57172c5465855457a602ed99514df968ac060aee
Shimaa Badawy <sweetgirlloly96@gmail.com>
2023-03-03 16:57:39 +0000

    Add segment

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,12 +1,19 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
+    segment = xray_recorder.begin_segment('user_activities')
+    
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
+
+    d = {'now' : now.isoformat()}
+    segment.put_metadata('key', d , 'namespace')
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
